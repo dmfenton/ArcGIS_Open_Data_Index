e49053d3549e379d9dd0a3c5d9d7908f0c4fcb26
dmfenton <dfenton@esri.com>
2016-06-14 22:21:37 -0400

    make logstash set up less fragile

--- logstashEntry.sh
+++ logstashEntry.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -e
 curl --silent -XPUT http://elasticsearch:9200/opendata --data-binary @mapping.json
-gunzip /tmp/ArcGIS_Open_Data_Index.txt.gz && cat /tmp/ArcGIS_Open_Data_Index.txt | logstash -f /tmp/logstash.conf
+pv /tmp/ArcGIS_Open_Data_Index.txt.gz | gunzip | logstash -f /tmp/logstash.conf
